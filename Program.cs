7190aca4a36c90121ddcab86dc31911aae72076e
Gabriel <ggabrielcunha06@gmail.com>
2025-05-18 13:05:29 -0300

    correção na das escritas e do laço

--- Program.cs
+++ Program.cs
@@ -2,24 +2,28 @@
 
 Console.WriteLine("------ Bem-vindo(a) programas de horas extras ------");
 
-do {
-    Console.Write("Digite seu salario/hora (em R$): ");
-} while(!double.TryParse(Console.ReadLine(), out salarioHora) || salarioHora <= 0);
+Console.Write("Digite seu salario/hora (em R$): ");
+while(!double.TryParse(Console.ReadLine(), out salarioHora) || salarioHora <= 0){
+    Console.WriteLine("Número inválido! Digite novamente seu salario/hora (em R$):");
+}
 
-do {
-    Console.Write("Digite o total de horas trabalhadas: ");
-} while (!double.TryParse(Console.ReadLine(), out horasTrabalhadas) || horasTrabalhadas < 0);
 
-do {
-    Console.Write("Digite o número de horas extras trabalhadas: ");
-} while (!double.TryParse(Console.ReadLine(), out horasExtras) || horasExtras < 0);
+Console.Write("Digite o total de horas trabalhadas: ");
+while (!double.TryParse(Console.ReadLine(), out horasTrabalhadas) || horasTrabalhadas < 0){
+    Console.WriteLine("Número inválido! Digite novamente o total de horas trabalhadas (números positivos):");
+}
 
-double salarioFinal = salarioHora * horasTrabalhadas;
-double salarioFinalHorasExtras = CalcularSalario(salarioHora, horasTrabalhadas, horasExtras);
+Console.Write("Digite o número de horas extras trabalhadas: ");
+while (!double.TryParse(Console.ReadLine(), out horasExtras) || horasExtras < 0) {
+    Console.WriteLine("Número inválido! Digite novamente o total de horas extras trabalhadas (números positivos):");
+}
 
-Console.WriteLine($"Salario total sem acrescimo de horas extras: {salarioFinal:C2}");
+double salarioSemHorasExtras = salarioHora * horasTrabalhadas;
+double salarioFinal = CalcularSalario(salarioHora, horasTrabalhadas, horasExtras);
 
-Console.WriteLine($"Salario total com acrescimo de horas extras: {salarioFinalHorasExtras:C2}");
+Console.WriteLine($"Salario total sem acréscimo de horas extras: {salarioSemHorasExtras:C2}");
+
+Console.WriteLine($"Salario total com acréscimo de horas extras: {salarioFinal:C2}");
 
 Console.Write("\nPressione qualquer tecla para finalizar o programa!");
 Console.ReadKey();
